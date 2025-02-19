6ebc21312e9964f495bed62633fb8fae4dc1440f
syamsul_492 <syamsuldahlan07@gmail.com>
2025-02-19 17:47:13 +0700

    menambahkan file perkenalan

---
+++ team/syamsul.py
@@ -0,0 +1,14 @@
+# Perkenalan Syamsul Dahlan
+def perkenalan():
+    nama = "Syamsul Dahlan"
+    nim = "2213010492"
+    peran = "Pengatur data dan informasi"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya bertugas mengelola data dan informasi dalam aplikasi.\n"
+        "Dalam istilah sederhana, saya memastikan semua data tersimpan dengan aman dan bisa diakses saat dibutuhkan."
+    )
+    print(deskripsi)
+
+if __name__ == "__main__":
+    perkenalan()
