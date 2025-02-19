80ac8a7a4aa68f8bac3da2c35f7c068728a554b7
Tegarrisky <tegarrisky05@gmail.com>
2025-02-19 22:42:27 +0700

    Create Tegar

---
+++ team/tegar.py
@@ -0,0 +1,14 @@
+# Perkenalan Tegar Risky Nugrahanto
+def perkenalan():
+    nama = "Tegar Risky Nugrahanto"
+    nim = "2213010486"
+    peran = "Pengatur tampilan aplikasi"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya bertugas mengatur tampilan aplikasi sehingga terlihat rapi dan mudah digunakan.\n"
+        "Secara sederhana, saya membuat agar pengguna merasa nyaman saat melihat dan menggunakan aplikasi."
+    )
+    print(deskripsi)
+
+if __name__ == "__main__":
+    perkenalan()
